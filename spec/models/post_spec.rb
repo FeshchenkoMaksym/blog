868ffec5781116dda8f2a04197b212c41249c763
FeshchenkoMaksym <maksym.feshchenko93@gmail.com>
2018-10-31 13:31:30 +0200

    fix post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,25 +9,25 @@ RSpec.describe Post, type: :model do
 
   it { is_expected.to be_valid }
 
-  describe 'when author is not present' do
+  context 'when author is not present' do
     before { post_1.author = '' }
 
     it { is_expected.to be_invalid }
   end
 
-  describe 'when title is too long' do
+  context 'when title is too long' do
     before { post_1.title = 'a' * 32 }
 
     it { is_expected.to be_invalid }
   end
 
-  describe 'when title is too short' do
+  context 'when title is too short' do
     before { post_1.title = 'a' }
 
     it { is_expected.to be_invalid }
   end
 
-  describe 'when text is too short' do
+  context 'when text is too short' do
     before { post_1.text = 'a' }
 
     it { is_expected.to be_invalid }
